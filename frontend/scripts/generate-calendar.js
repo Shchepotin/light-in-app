6f04e2c5de520ace835f3471d82dbe0bdcd60e67
Vladyslav Shchepotin <vladislav.schepotin@gmail.com>
2024-06-08 03:29:50 +0300

    feat: new schedule

--- frontend/scripts/generate-calendar.js
+++ frontend/scripts/generate-calendar.js
@@ -1,5 +1,6 @@
 const { writeFileSync } = require("fs");
 const ics = require("ics");
+const { fromZonedTime } = require("date-fns-tz");
 const scheduleData = require("../src/data.json");
 
 function generateICSCalendar(data, queue) {
@@ -14,23 +15,46 @@ function generateICSCalendar(data, queue) {
       .queue[queue - 1].schedule.map((item) => {
         const currentDate = new Date(today + index * 24 * 60 * 60 * 1000);
         const endTime = item.end === "24:00" ? "23:59" : item.end;
+
+        const start = fromZonedTime(
+          new Date(
+            currentDate.getFullYear(),
+            currentDate.getMonth(),
+            currentDate.getDate(),
+            Number(item.start.split(":")[0]),
+            Number(item.start.split(":")[1])
+          ),
+          "Europe/Kiev"
+        );
+
+        const end = fromZonedTime(
+          new Date(
+            currentDate.getFullYear(),
+            currentDate.getMonth(),
+            currentDate.getDate(),
+            Number(endTime.split(":")[0]),
+            Number(endTime.split(":")[1])
+          ),
+          "Europe/Kiev"
+        );
+
         return {
           calName: `Відключення електропостачання`,
           title: `Вимкнення світла`,
           url: "https://light-in-app.web.app/",
           start: [
-            currentDate.getFullYear(),
-            currentDate.getMonth() + 1,
-            currentDate.getDate(),
-            Number(item.start.split(":")[0]),
-            Number(item.start.split(":")[1]),
+            start.getFullYear(),
+            start.getMonth() + 1,
+            start.getDate(),
+            start.getHours(),
+            start.getMinutes(),
           ],
           end: [
-            currentDate.getFullYear(),
-            currentDate.getMonth() + 1,
-            currentDate.getDate(),
-            Number(endTime.split(":")[0]),
-            Number(endTime.split(":")[1]),
+            end.getFullYear(),
+            end.getMonth() + 1,
+            end.getDate(),
+            end.getHours(),
+            end.getMinutes(),
           ],
         };
       });
